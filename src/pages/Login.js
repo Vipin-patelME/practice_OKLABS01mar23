355e84ca6d1eb4c7300184ba5ab07a85e53fe82f
Vipin-patelME <t4t.forex@gmail.com>
2023-03-01 23:02:31 -0800

    strapi api call problem fixed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,24 +12,46 @@ export default function Login() {
     const onPasswordInput = e =>{
         setPassword(e.target.value)
     }
+    const onUserLogin = e =>{
+        e.preventDefault()
+        const url = "http://localhost:1337/api/auth/local"
+        const userData = {
+            "identifier": username,
+            "password": password
+          }
+          
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userData)
+        };
 
+        fetch(url, options)
+        .then((res =>{
+            return res.json()
+        }))
+        .then((data =>{
+            console.log(data)
+        }))
+        .catch()
+    }
     return (
         <div className='login-page'>
-            <form className='login-form'>
+            <form onSubmit={onUserLogin} className='login-form'>
                 <div className="mb-3 row">
-                    <label htmlFor="staticEmail" className="col-sm-2 col-form-label">User name</label>
+                    <label htmlFor="username" className="col-sm-2 col-form-label">User name</label>
                     <div className="col-sm-10">
-                    <input type="text" onChange={onUserNameInput} value={username}  className="form-control" id="staticEmail"  />
+                        <input type="text" onChange={onUserNameInput} value={username}  className="form-control" id="username"  />
                     </div>
                 </div>
                 <div className="mb-3 row">
                     <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
                     <div className="col-sm-10">
-                    <input type="password" onChange={onPasswordInput} value={password} className="form-control" id="inputPassword" />
+                        <input type="password" onChange={onPasswordInput} value={password} className="form-control" id="inputPassword" />
                     </div>
                 </div>
                 <div className="mb-3 row w-25">
-                    <button type='button' className='btn btn-primary'>Login</button>
+                    <button type='submit' className='btn btn-primary'>Login</button>
                 </div>
                 
             </form>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 
 export default function Register() {
 
-
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
 
     const onUserNameInput = e =>{
         setUsername(e.target.value)
@@ -13,14 +13,16 @@ export default function Register() {
     const onPasswordInput = e =>{
         setPassword(e.target.value)
     }
+    const onEmailInput = e =>{
+        setEmail(e.target.value)
+    }
 
     const onSubmitUserDetails = ()=>{
-        const url = "http://localhost:1337/api/userlogins"
+        const url = "http://localhost:1337/api/auth/local/register"
         const userData = {
-            "data": {
-              "username": username,
-              "password": password
-            }
+            "username": username,
+            "email": email,
+            "password": password
           }
         const options = {
             method: 'POST',
@@ -28,22 +30,31 @@ export default function Register() {
             body: JSON.stringify(userData)
         };
 
+        console.log(username)
+
         fetch(url, options)
         .then((res =>{
             return res.json()
         }))
         .then((data =>{
             console.log(data)
-        })).catch()
+        }))
+        .catch()
     }
 
     return (
         <div className='login-page'>
-            <form onSubmit={onSubmitUserDetails} className='login-form'>
+            <form  className='login-form'>
                 <div className="mb-3 row">
                     <label htmlFor="staticEmail" className="col-sm-2 col-form-label">User name</label>
                     <div className="col-sm-10">
                     <input type="text" onChange={onUserNameInput}  value={username} className="form-control" id="staticEmail"  />
+                    </div>
+                </div>
+                <div className="mb-3 row">
+                    <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
+                    <div className="col-sm-10">
+                    <input type="email" onChange={onEmailInput} value={email} className="form-control" id="inputEmail" />
                     </div>
                 </div>
                 <div className="mb-3 row">
@@ -53,7 +64,7 @@ export default function Register() {
                     </div>
                 </div>
                 <div className="mb-3 row w-25">
-                    <button type='submit' className='btn btn-primary'>Create Account</button>
+                    <input onClick={onSubmitUserDetails} className='btn btn-primary' value = "Create Account" />
                 </div>
             </form>
         </div>
